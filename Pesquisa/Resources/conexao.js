7b58f59e57e53696f9d25992acc4ba7ea3981392
Gil Beyruth <firstpixel@gmail.com>
2016-07-15 17:01:32 -0300

    Update conexao.js
    Added callbacks for onSuccess and onError.

--- Pesquisa/Resources/conexao.js
+++ Pesquisa/Resources/conexao.js
@@ -1,14 +1,16 @@
-exports.getJson = function(arg) {
+exports.getJson = function(onSuccess,onError,arg) {
 	var json,url,xhr;
 	url = "https://raw.githubusercontent.com/lukasepramos/Pesquisa/master/jsonQuestionario.txt"; //Criar url com json
 	xhr = Ti.Network.createHTTPClient({
 	    onload: function() {
-	    json = JSON.parse(this.responseText);
+		    json = JSON.parse(this.responseText);
+		    onSuccess(json);
 	    },
 	    onerror: function(e) {
 		Ti.API.debug("STATUS: " + this.status);
 		Ti.API.debug("TEXT:   " + this.responseText);
 		Ti.API.debug("ERROR:  " + e.error);
+		onError();
 		alert('There was an error retrieving the remote data. Try again.');
 	    },
 	    timeout:5000
